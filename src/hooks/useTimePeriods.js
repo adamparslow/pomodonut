3829f221b94eedf50ab2ba90304b5b6bf82c1299
Adam Parslow <adam.parslow@hotmail.com>
2023-01-05 03:08:17 +1100

    Clean up use time periods

--- src/hooks/useTimePeriods.js
+++ src/hooks/useTimePeriods.js
@@ -5,37 +5,37 @@ const useTimePeriods = () => {
 
    const increment = () => {
       setTimePeriods(parseInt(timePeriods) + 1);
-   }
+   };
 
    const decrement = () => {
       const newVal = timePeriods > 0 ? timePeriods - 1 : 0;
       setTimePeriods(newVal);
-   }
+   };
 
    const reset = () => {
       setTimePeriods(0);
-   }
+   };
 
-   const finishDate = new Date(Date.now() + 30 * timePeriods * 60 * 1000);
-   let end = "am";
-   let hours = finishDate.getHours();
-   if (hours >= 12) {
-      if (hours > 12) {
-         hours -= 12;
-      }
-      end = "pm";
-   } else if (hours === 0) {
-      hours = 12;
-   }
-   let minutes = finishDate.getMinutes() >= 30 ? 30 : 0;
+   // const finishDate = new Date(Date.now() + 30 * timePeriods * 60 * 1000);
+   // let end = "am";
+   // let hours = finishDate.getHours();
+   // if (hours >= 12) {
+   //    if (hours > 12) {
+   //       hours -= 12;
+   //    }
+   //    end = "pm";
+   // } else if (hours === 0) {
+   //    hours = 12;
+   // }
+   // let minutes = finishDate.getMinutes() >= 30 ? 30 : 0;
 
-   const timeToEnd = `${hours}:${minutes.toString().padStart(2, '0')} ${end}`
+   // const timeToEnd = `${hours}:${minutes.toString().padStart(2, '0')} ${end}`
 
    return {
       value: timePeriods,
       increment,
       decrement,
-      reset
+      reset,
    };
 };
 
