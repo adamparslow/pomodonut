0f3a262630395aaaaaffbcbb1d11a651b3089d77
Adam Parslow <adam.parslow@hotmail.com>
2023-01-05 03:04:22 +1100

    Add animations for time wheel

--- src/App.js
+++ src/App.js
@@ -1,127 +1,147 @@
-import './App.css';
-import { useEffect } from 'react';
-import TimerWheel from './TimerWheel';
-import NumberInput from './NumberInput';
-import useTimePeriods from './hooks/useTimePeriods';
-import useIsMuted from './hooks/useIsMuted';
-import useTime from './hooks/useTime';
-import styled from 'styled-components';
-import Button from './Button';
-import { FaUndoAlt, FaVolumeUp, FaVolumeMute } from 'react-icons/fa';
+import "./App.css";
+import { useEffect } from "react";
+import TimerWheel from "./TimerWheel";
+import NumberInput from "./NumberInput";
+import useTimePeriods from "./hooks/useTimePeriods";
+import useIsMuted from "./hooks/useIsMuted";
+import useTime from "./hooks/useTime";
+import styled from "styled-components";
+import Button from "./Button";
+import { FaUndoAlt, FaVolumeUp, FaVolumeMute } from "react-icons/fa";
 
 const colours = {
-  "WORK": {
-    primary: "#ff7150",
-    background: "#ffaa4d"
-  },
-  "DELAYED START": {
-    primary: "#ff7150",
-    background: "#ffaa4d"
-  },
-  "BREAK": {
-    primary: "#54b574",
-    background: "#95d8c2"
-  }
-}
+   WORK: {
+      primary: "#ff7150",
+      background: "#ffaa4d",
+   },
+   "DELAYED START": {
+      primary: "#ff7150",
+      background: "#ffaa4d",
+   },
+   BREAK: {
+      primary: "#54b574",
+      background: "#95d8c2",
+   },
+};
 
 const Background = styled.div`
-  height: 100vh;
-  width: 100vw;
+   height: 100vh;
+   width: 100vw;
 
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
 
-  @media only screen and (max-width: 700px) {
-    flex-direction: column-reverse;
-  }
+   @media only screen and (max-width: 700px) {
+      flex-direction: column-reverse;
+   }
 `;
 
 const Inner = styled.div`
-  width:90%;
-  height:90%;
-  border-radius: 50%;
-  background-color: white;
-  
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap:40px;
+   width: 90%;
+   height: 90%;
+   border-radius: 50%;
+   background-color: white;
+
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   gap: 40px;
 `;
 
 const TypeTitle = styled.h1`
-  font-size: 3em;
-  margin:0;
-  height: 50px;
+   font-size: 3em;
+   margin: 0;
+   height: 50px;
 `;
 
 const TimerTitle = styled.h1`
-  font-size: 5em;
-  margin:0;
-  color: ${props => props.colour};
+   font-size: 5em;
+   margin: 0;
+   color: ${(props) => props.colour};
 `;
 
 const ButtonTray = styled.div`
-  display: flex;
-  gap: 40px;
-  margin: 30px;
+   display: flex;
+   gap: 40px;
+   margin: 30px;
 
-  @media only screen and (min-width: 701px) {
-    align-self: flex-end;
-    width: 250px;
-  }
+   @media only screen and (min-width: 701px) {
+      align-self: flex-end;
+      width: 250px;
+   }
 `;
 
 const EndTime = styled.p`
-  font-size: 1.5em;
-  text-align: center;
-  margin: 30px;
+   font-size: 1.5em;
+   text-align: center;
+   margin: 30px;
 
-  @media only screen and (min-width: 701px) {
-    align-self: flex-end;
-    width: 250px;
-  }
+   @media only screen and (min-width: 701px) {
+      align-self: flex-end;
+      width: 250px;
+   }
 `;
 
 function App() {
-  const { timeText, percentage, type, setDelayedStart, isFirstDelayed } = useTime();
-  const { timePeriods, incrementTimePeriods, decrementTimePeriods, resetTimePeriods, timeToEnd } = useTimePeriods();
-  const { isMuted, toggleMute, playAlert } = useIsMuted();
+   const { timeText, percentage, type, setDelayedStart, isFirstDelayed } =
+      useTime();
+   const { value, increment, decrement, reset, timeToEnd } = useTimePeriods();
+   const { isMuted, toggleMute, playAlert } = useIsMuted();
 
-  useEffect(() => {
-    if (type === "WORK") {
-      decrementTimePeriods();
-    }
+   useEffect(() => {
+      if (type === "WORK") {
+         decrement();
+      }
 
-    playAlert();
-  }, [type, decrementTimePeriods, playAlert]);
+      // playAlert();
+   }, [type, decrement, playAlert]);
 
-  const background = colours[type].background;
-  const primary = colours[type].primary;
+   const background = colours[type].background;
+   const primary = colours[type].primary;
 
-  return (
-    <Background style={{ backgroundColor: background }}>
-      <ButtonTray>
-        <Button colour={primary} onClick={resetTimePeriods}><FaUndoAlt /></Button>
-        <Button colour={primary} onClick={toggleMute}>
-          {isMuted ? <FaVolumeMute /> : <FaVolumeUp />}
-        </Button>
-        <button onClick={setDelayedStart}>Delay Start</button>
-        <p>{isFirstDelayed ? "yes" : "no"}</p>
-      </ButtonTray>
-      <TimerWheel percentage={timePeriods > 0 ? percentage : 0} wheelColour={primary} backgroundColour={background}>
-        <Inner>
-          <TypeTitle>{timePeriods > 0 ? type : "Set value to start"}</TypeTitle>
-          <TimerTitle colour={primary} style={{ opacity: timePeriods > 0 ? '100%' : '50%' }}>{timeText}</TimerTitle>
-          <NumberInput colour={primary} value={timePeriods} increment={incrementTimePeriods} decrement={decrementTimePeriods}></NumberInput>
-        </Inner >
-      </TimerWheel>
-      <EndTime>End time: <span style={{ color: 'white' }}>{timePeriods > 0 ? timeToEnd : "00:00"}</span></EndTime>
-    </Background>
-  );
+   return (
+      <Background style={{ backgroundColor: background }}>
+         <ButtonTray>
+            <Button colour={primary} onClick={reset}>
+               <FaUndoAlt />
+            </Button>
+            <Button colour={primary} onClick={toggleMute}>
+               {isMuted ? <FaVolumeMute /> : <FaVolumeUp />}
+            </Button>
+            <button onClick={setDelayedStart}>Delay Start</button>
+            <p>{isFirstDelayed ? "yes" : "no"}</p>
+         </ButtonTray>
+         <TimerWheel
+            percentage={percentage}
+            wheelColour={primary}
+            backgroundColour={background}
+         >
+            <Inner>
+               <TypeTitle>{value > 0 ? type : "Set value to start"}</TypeTitle>
+               <TimerTitle
+                  colour={primary}
+                  style={{ opacity: value > 0 ? "100%" : "50%" }}
+               >
+                  {timeText}
+               </TimerTitle>
+               <NumberInput
+                  colour={primary}
+                  value={value}
+                  increment={increment}
+                  decrement={decrement}
+               ></NumberInput>
+            </Inner>
+         </TimerWheel>
+         <EndTime>
+            End time:{" "}
+            <span style={{ color: "white" }}>
+               {value > 0 ? timeToEnd : "00:00"}
+            </span>
+         </EndTime>
+      </Background>
+   );
 }
-
-
 
 export default App;
--- src/TimerWheel.js
+++ src/TimerWheel.js
@@ -1,30 +1,52 @@
-import styled from "styled-components";
-import { useRef } from "react";
+import styled, { keyframes } from "styled-components";
 
 const Wheel = styled.div`
-  height: 90vw;
-  max-height: 700px;
-  aspect-ratio: 1;
-  border-radius: 50%;
+   height: 90vw;
+   max-height: 700px;
+   aspect-ratio: 1;
+   border-radius: 50%;
 
-  position: relative;
+   position: relative;
 
-  display:flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
 `;
 
 const Gap = styled.div`
-  height: 85%; 
-  width: 85%;
+   height: 85%;
+   width: 85%;
 
-  border-radius: 50%;
+   border-radius: 50%;
 
-  display:flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+`;
+
+const rotationAnimation = (start, end) => keyframes`
+   0%   {transform: rotate(${start}turn);}
+   100%   {transform: rotate(${end}turn);}
+`;
+
+const Rotator = styled.div`
+   height: 100%;
+   width: 100%;
+   position: absolute;
+   border-radius: 50%;
+
+   display: flex;
+   justify-content: center;
+
+   animation-duration: 1s;
+   animation-timing-function: linear;
+   animation-iteration-count: infinite;
+
+   animation: ${(props) =>
+         rotationAnimation(props.turn - 1 / (25 * 60), props.turn)}
+      1s linear infinite;
 `;
 
 const Circle = styled.div`
@@ -44,50 +66,36 @@ function TimerWheel(props) {
    const wheelColour = props.wheelColour;
    const backgroundColour = props.backgroundColour;
 
-   const wheelRef = useRef();
-   const circleRef = useRef();
-
-   let radius = 323.9;
-   if (wheelRef.current) {
-      radius = (wheelRef.current.offsetWidth - circleRef.current.offsetWidth) / 2;
-   }
-   const [top, left] = calculateCirclePos(percentage, radius);
-
    return (
-      <Wheel ref={wheelRef} style={
-         {
-            background: percentage <= 0.5 ?
-               `linear-gradient(${degree}deg, transparent 50%, ${wheelColour} 50%),
-               linear-gradient(90deg, ${wheelColour} 50%, transparent 50%)` :
-               `linear-gradient(90deg, transparent 50%, ${backgroundColour} 50%),
-               linear-gradient(${degree}deg, transparent 50%, ${wheelColour} 50%)`
-         }
-      }>
-         <Circle style={
-            {
-               backgroundColor: wheelColour,
-               top: 0
-            }
-         }></Circle>
-         <Circle ref={circleRef} style={
-            {
-               backgroundColor: wheelColour,
-               top: top,
-               left: left
-            }
-         }></Circle>
+      <Wheel
+         style={{
+            background:
+               percentage <= 0.5
+                  ? `linear-gradient(${degree}deg, transparent 50%, ${wheelColour} 50%),
+               linear-gradient(90deg, ${wheelColour} 50%, transparent 50%)`
+                  : `linear-gradient(90deg, transparent 50%, ${backgroundColour} 50%),
+               linear-gradient(${degree}deg, transparent 50%, ${wheelColour} 50%)`,
+         }}
+      >
+         <Rotator turn={percentage}>
+            <Circle
+               style={{
+                  backgroundColor: wheelColour,
+                  zIndex: 10,
+               }}
+            ></Circle>
+         </Rotator>
          <Gap style={{ backgroundColor: backgroundColour }}>
             {props.children}
          </Gap>
+         <Circle
+            style={{
+               backgroundColor: wheelColour,
+               top: 0,
+            }}
+         ></Circle>
       </Wheel>
    );
 }
-
-function calculateCirclePos(percentage, radius) {
-   const top = (-Math.cos(Math.PI * 2 * percentage) * radius) + radius;
-   const left = Math.sin(Math.PI * 2 * percentage) * radius + radius;
-
-   return [top, left]
-};
 
 export default TimerWheel;
